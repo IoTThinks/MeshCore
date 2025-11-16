a6ba4d262accf34732817f441fcaa23ff2a9976c
IoTThinks <vinhdat82@gmail.com>
2025-11-16 12:25:57 +0700

    Update MyMesh.h
    Fixed OTA

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -68,11 +68,11 @@ struct NeighbourInfo {
 };
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "15 Nov 2025"
+  #define FIRMWARE_BUILD_DATE   "16 Nov 2025"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.10.0 - Variant IoTThinks.com - powersaving03"
+  #define FIRMWARE_VERSION   "v1.10.0 - Variant IoTThinks.com - PowerSaving04"
 #endif
 
 #define FIRMWARE_ROLE "repeater"
