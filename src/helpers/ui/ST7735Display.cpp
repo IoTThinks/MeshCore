4aabd1babea3b7f80adb81b1ad2338e42bccd5fe
Kevin Le <vinhdat82@gmail.com>
2026-06-07 20:40:48 +0700

    Prevented back-powering to save 2.8 mA in ST7735Display

--- src/helpers/ui/ST7735Display.cpp
+++ src/helpers/ui/ST7735Display.cpp
@@ -61,12 +61,13 @@ void ST7735Display::turnOff() {
     digitalWrite(PIN_TFT_LEDA_CTL, LOW); 
 #endif
 
-    // Prevent back-powering to save 2.5 mA
+    // Prevent back-powering to save 2.8 mA
     pinMode(PIN_TFT_CS, INPUT);
     pinMode(PIN_TFT_DC, INPUT);
     pinMode(PIN_TFT_SDA, INPUT);
     pinMode(PIN_TFT_SCL, INPUT);
     pinMode(PIN_TFT_RST, INPUT);
+    pinMode(PIN_TFT_LEDA_CTL, INPUT);
 
     _isOn = false;
 
